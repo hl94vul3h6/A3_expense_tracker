9af3666099986cd5ac12960b3fc7a55519427f20
hl94vul3h6 <gary6780gary6780@gmail.com>
2023-02-15 15:30:34 -0800

    feat: add message partials

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const exphbs = require("express-handlebars");
 const Record = require('./models/record')
 const bodyParser = require("body-parser")
 const methodOverride = require('method-override')
+const flash = require("connect-flash");
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -35,10 +36,14 @@ app.use(methodOverride("_method"));
 
 usePassport(app);
 
+app.use(flash());
+
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated();
   res.locals.user = req.user;
+  res.locals.success_msg = req.flash("success_msg"); 
+  res.locals.warning_msg = req.flash("warning_msg");
   next();
 });
 

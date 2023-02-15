5fcb0034a59816476e1298564ccb61b5595b4508
hl94vul3h6 <gary6780gary6780@gmail.com>
2023-02-15 14:23:14 -0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,12 @@ const router = express.Router();
 // 引入 home 模組程式碼
 const home = require('./modules/home')
 const records = require("./modules/records");
+const users = require('./modules/users')
 
 // 將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/', home)
 router.use("/records", records);
+router.use("/users", users)
 
 // 匯出路由器
 module.exports = router;

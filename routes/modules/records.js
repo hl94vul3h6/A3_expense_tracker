25b9adfdd5a60714648b01c6c48fd0a78bd8f8ee
hl94vul3h6 <gary6780gary6780@gmail.com>
2023-02-16 14:33:10 -0800

    modify the new, index, edit, main.hbs with the looking of categories and records.js with create and finish create function

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const Record = require("../../models/record");
+const Category = require("../../models/category");
 
 //拿New樣板
 router.get("/new", (req, res) => {
-  return res.render("new");
+  Category.find()
+    .lean()
+    .sort({ _id: "asc" })
+    .then((categories) => {
+      return res.render("new", { categories });
+    })
+    .catch((error) => console.log(error));
 });
 
 //接住表單資料 Create
@@ -20,9 +27,13 @@ router.post("/", (req, res) => {
 router.get("/:id/edit", (req, res) => {
   const userId = req.user._id;
   const _id = req.params.id;
-  return Record.findOne({ _id, userId})
-    .lean()
-    .then((record) => res.render("edit", { record }))
+  Promise.all([
+    Category.find().lean().sort({ _id: "asc" }),
+    Record.findOne({ _id, userId }).lean().sort({ date: "desc" }),
+  ])
+    .then((results) => {
+      return res.render("edit", { record: results[1], categories: results[0] });
+    })
     .catch((error) => console.log(error));
 });
 
